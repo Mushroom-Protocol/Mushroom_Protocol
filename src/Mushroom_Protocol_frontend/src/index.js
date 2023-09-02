1dbe324a01433db165ddc6abc93d39e736173cb1
ArielRobotti <arielrobotti@gmail.com>
2023-09-01 23:10:21 -0300

    Initial commit.

---
+++ src/Mushroom_Protocol_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { Mushroom_Protocol_backend } from "../../declarations/Mushroom_Protocol_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Mushroom_Protocol_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
