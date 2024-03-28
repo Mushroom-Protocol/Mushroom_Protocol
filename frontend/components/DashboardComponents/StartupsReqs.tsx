2e3cdad61afb505a9b2592a8f5b5050a73fbaca6
Daniel Benitez <da8y_01@yahoo.com>
2024-03-28 17:37:27 -0500

    wip: Implement approve incoming startup on StartupsReqs.

--- frontend/components/DashboardComponents/StartupsReqs.tsx
+++ frontend/components/DashboardComponents/StartupsReqs.tsx
@@ -1,62 +1,90 @@
 import React, { useEffect, useState } from 'react';
 import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Image, List, ListItem, Stack, Text } from '@chakra-ui/react';
-import { useCanister } from "@connect2ic/react";
+import { useCanister, useConnect } from "@connect2ic/react";
+
+interface IncomingStartUp {
+  startUpName : string;
+  email : string;
+  website : string;
+  startUpSlogan : string;
+  shortDes : string;
+  logo : [];
+  startupStatus : string;
+  tlr : number;
+  fullNameTl : string;
+  specializationTL : string;
+  linkedinTL : string;
+  industry : string;
+  country : string;
+};
+
+interface Startup {
+  owner: object;
+  admissionDate: number;
+  startupId: string;
+  startUpName: string;
+  email: string;
+  website: string;
+  startUpSlogan: string;
+  shortDes: string;
+  logo: [];
+  documents: [[]];
+  startupStatus: string;
+  tlr: number;
+  fullNameTl: string;
+  specializationTL: string;
+  linkedinTL: string;
+  industry: string;
+  country: string;
+  valoration: number;
+  projects: [string];
+}
+
+const initialStateStartups = [
+  {
+    owner: {},
+    admissionDate: 0,
+    startupId: "",
+    startUpName: "",
+    email: "",
+    website: "",
+    startUpSlogan: "",
+    shortDes: "",
+    logo: [],
+    documents: [[]],
+    startupStatus: "",
+    tlr: 0,
+    fullNameTl: "",
+    specializationTL: "",
+    linkedinTL: "",
+    industry: "",
+    country: "",
+    valoration: 0,
+    projects: [""]
+  }
+] as [Startup]
+
+function blobToBase64(buffer: Uint8Array) {
+  var binary = '';
+  var bytes = new Uint8Array(buffer);
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
 
 const StartupsReqs: React.FC = () => {
-  const [backend] = useCanister("backend");
-  const [startups, setStartups] = useState<[
-    {
-      startUpName: string;
-      email: string;
-      website: string;
-      startUpSlogan: string;
-      shortDes: string;
-      logo: [];
-      startupStatus: string;
-      tlr: number;
-      fullNameTl: string;
-      specializationTL: string;
-      linkedinTL: string;
-      industry: string;
-      country: string;
-    }
-  ] | null>([
-    {
-      startUpName: "",
-      email: "",
-      website: "",
-      startUpSlogan: "",
-      shortDes: "",
-      logo: [],
-      startupStatus: "",
-      tlr: 0,
-      fullNameTl: "",
-      specializationTL: "",
-      linkedinTL: "",
-      industry: "string",
-      country: ""
-    }
-  ]);
+  const [backend] = useCanister("backend")
+  const {principal} = useConnect()
+  const [startups, setStartups] = useState<[Startup] | null>(initialStateStartups)
 
   useEffect(() => {
     const getIncomingStartUps = async () => {
       try {
         const response = await backend.getIncomingStartUps()
-        await setStartups(response as [{
-          startUpName: string;
-          email: string;
-          website: string;
-          startUpSlogan: string;
-          shortDes: string;
-          logo: [];
-          startupStatus: string;
-          tlr: number;
-          fullNameTl: string;
-          specializationTL: string;
-          linkedinTL: string;
-          industry: string;
-          country: string;
-        }])
+        console.log(response)
+        await setStartups(response as [Startup])
       } catch (error) {
         console.error('Error al obtener datos de startups:', error)
       }
@@ -64,6 +92,16 @@ const StartupsReqs: React.FC = () => {
 
     getIncomingStartUps();
   }, []);
+
+  const approveStartUp = async (owner) => {
+    console.log("approveStartUp")
+    const resGetIncomingStartupByOwner = await backend.getIncomingStartupByOwner(owner);
+    const resGetIncomingStartupByOwnerOk = resGetIncomingStartupByOwner['ok']
+    console.log(resGetIncomingStartupByOwnerOk)
+    const valoration = 4
+    const resApproveStartUp = await backend.approveStartUp(resGetIncomingStartupByOwnerOk, valoration, owner)
+    console.log(resApproveStartUp)
+  }
 
   return (
     <>
@@ -91,7 +129,7 @@ const StartupsReqs: React.FC = () => {
               <Divider />
               <CardFooter>
                 <ButtonGroup spacing='2'>
-                  <Button variant='ghost' colorScheme='blue'>
+                  <Button variant='ghost' colorScheme='blue' onClick={() => approveStartUp(startup.owner)}>
                     Approve
                   </Button>
                 </ButtonGroup>
