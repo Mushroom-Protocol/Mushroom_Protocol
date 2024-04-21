5133554548b836f526f86e13873fcc5fad56e3ed
Daniel Benitez <da8y_01@yahoo.com>
2024-04-20 20:58:25 -0500

    wip: Format Document on ColecctionForm.

--- frontend/components/Apply/ColecctionForm.tsx
+++ frontend/components/Apply/ColecctionForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import {
   Box,
   Center,
@@ -21,32 +21,31 @@ import {
   InputRightElement,
   Tooltip,
   useToast,
-} from '@chakra-ui/react';
-import { AiFillPicture } from "react-icons/ai";
-import { IoInformationCircleOutline } from "react-icons/io5";
+} from "@chakra-ui/react"
+import { AiFillPicture } from "react-icons/ai"
+import { IoInformationCircleOutline } from "react-icons/io5"
 import { useCanister } from "@connect2ic/react"
-
 
 const ColecctionForm = () => {
   const [backend] = useCanister("backend")
-  const { isOpen, onToggle } = useDisclosure();
-  const toast = useToast();
- 
+  const { isOpen, onToggle } = useDisclosure()
+  const toast = useToast()
+
   interface FormData {
-    collectionName: string;
-    shortStorytelling: string;
-    storytellingCollection: string;
-    totalSupply: string;
-    distribution: string;
-    utilities: string[];
-    tokenPrices: string;
-    documents: string;
-    typesImages: string;
-    nftImages: string;
-    creator: string;
-    collectionStatus: string;
+    collectionName: string
+    shortStorytelling: string
+    storytellingCollection: string
+    totalSupply: string
+    distribution: string
+    utilities: string[]
+    tokenPrices: string
+    documents: string
+    typesImages: string
+    nftImages: string
+    creator: string
+    collectionStatus: string
   }
-  
+
   const initialFormData: FormData = {
     collectionName: "",
     shortStorytelling: "",
@@ -60,11 +59,9 @@ const ColecctionForm = () => {
     nftImages: "",
     creator: "",
     collectionStatus: "",
-  };
-  
-  const [formData, setFormData] = useState<FormData>(initialFormData);
+  }
 
-
+  const [formData, setFormData] = useState<FormData>(initialFormData)
 
   /*const [formData, setFormData] = useState({
     collectionName: "",
@@ -82,152 +79,180 @@ const ColecctionForm = () => {
   });*/
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
-    }));
-  };
+    }))
+  }
 
-    const handleCheckboxChange = (checkedValues: string[]) => {
+  const handleCheckboxChange = (checkedValues: string[]) => {
     setFormData({
       ...formData,
       utilities: checkedValues,
-    });
-  };
-
- 
+    })
+  }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  
-    let loadingToastId: string | number | undefined;
-  
+    event.preventDefault()
+
+    let loadingToastId: string | number | undefined
+
     try {
       // Muestra un toast de carga con formato sólido y color azul
       loadingToastId = toast({
-        title: 'Submitting Form',
-        status: 'loading', // 'loading' es el status para el estilo de carga
+        title: "Submitting Form",
+        status: "loading", // 'loading' es el status para el estilo de carga
         duration: null,
         isClosable: false,
-        variant: 'solid',
-      });
-  
-  
+        variant: "solid",
+      })
+
       ///////////// CORREGIR LLAMADO AL BACKEND FUNCION DE REGISTRO PROYECTO /////////////////////////
-      const response = await backend.whoAmi();
-  
+      const response = await backend.whoAmi()
+
       // Cierra el toast de carga cuando la acción se completa
       if (loadingToastId !== undefined) {
-        toast.close(loadingToastId);
+        toast.close(loadingToastId)
       }
-  
+
       // Muestra un toast de éxito con formato sólido y color verde
       toast({
-        title: 'Successful Submission',
-        description: 'Your form was submitted successfully.',
-        status: 'success', // 'success' es el status para el estilo de éxito
+        title: "Successful Submission",
+        description: "Your form was submitted successfully.",
+        status: "success", // 'success' es el status para el estilo de éxito
         duration: 5000,
         isClosable: true,
-        variant: 'solid',
-      });
-  
-      console.log(response); // ACTIVA TOAST DE MENSAJE DE SUBMIT ////////////////////////
+        variant: "solid",
+      })
+
+      console.log(response) // ACTIVA TOAST DE MENSAJE DE SUBMIT ////////////////////////
     } catch (error) {
       // Cierra el toast de carga cuando la acción falla
       if (loadingToastId !== undefined) {
-        toast.close(loadingToastId);
+        toast.close(loadingToastId)
       }
-  
+
       // Muestra un toast de error con formato sólido y color rojo
       toast({
-        title: 'Submission Error',
-        description: 'There was an error submitting the form. Please try again.',
-        status: 'error', // 'error' es el status para el estilo de error
+        title: "Submission Error",
+        description:
+          "There was an error submitting the form. Please try again.",
+        status: "error", // 'error' es el status para el estilo de error
         duration: 5000,
         isClosable: true,
-        variant: 'solid',
-      });
-  
-      console.error(error);
+        variant: "solid",
+      })
+
+      console.error(error)
     }
-  };
+  }
 
   return (
     <Center>
-      <Flex direction="row" align="center" gap={5} >
-      <Flex direction="column" align="center">
-        <Button
-          as={Flex}
-          direction="row"
-          alignItems="center"
-          bg="#000000"
-          borderColor="#1FAFC8"
-          borderWidth="1px"
-          p={10}
-          boxShadow="sm"
-          width="230px"
-          size="lg"
-          borderRadius="20px"
-          fontSize="16px"
-          _hover={{ bg: '#1FAFC8' }}
-          _active={{ bg: '#1FAFC8' }}
-          onClick={onToggle}
-        >
-          <Icon as={AiFillPicture} boxSize={12} marginRight={6} />
-          <Box>
-            <Text fontWeight="bold" fontSize="lg" color="white" >
-            Collection
-          </Text>
-          <Text fontWeight="bold" fontSize="lg" color="#737373">
-             Registration
-            </Text>
-          </Box>
-        </Button>
-
-        <Collapse in={isOpen} animateOpacity>
-
-          <Box
-            p="60px"
-            color="white"
-            mt="4"
+      <Flex direction="row" align="center" gap={5}>
+        <Flex direction="column" align="center">
+          <Button
+            as={Flex}
+            direction="row"
+            alignItems="center"
             bg="#000000"
+            borderColor="#1FAFC8"
             borderWidth="1px"
-            rounded="lg"
-        
-
+            p={10}
+            boxShadow="sm"
+            width="230px"
+            size="lg"
+            borderRadius="20px"
+            fontSize="16px"
+            _hover={{ bg: "#1FAFC8" }}
+            _active={{ bg: "#1FAFC8" }}
+            onClick={onToggle}
           >
-            <Heading>Collection Register Form</Heading>
-            <Text>*Enlists a collection of IP-NFTs and accesses fundraising in a decentralized fashion.</Text>
-            <br />
-            <form onSubmit={handleSubmit}>
-            <FormControl isRequired>
+            <Icon as={AiFillPicture} boxSize={12} marginRight={6} />
+            <Box>
+              <Text fontWeight="bold" fontSize="lg" color="white">
+                Collection
+              </Text>
+              <Text fontWeight="bold" fontSize="lg" color="#737373">
+                Registration
+              </Text>
+            </Box>
+          </Button>
+
+          <Collapse in={isOpen} animateOpacity>
+            <Box
+              p="60px"
+              color="white"
+              mt="4"
+              bg="#000000"
+              borderWidth="1px"
+              rounded="lg"
+            >
+              <Heading>Collection Register Form</Heading>
+              <Text>
+                *Enlists a collection of IP-NFTs and accesses fundraising in a
+                decentralized fashion.
+              </Text>
+              <br />
+              <form onSubmit={handleSubmit}>
+                <FormControl isRequired>
                   <FormLabel>Collection Name</FormLabel>
-                  <Input id="collectionName" name="collectionName" value={formData.collectionName} onChange={handleChange} placeholder="Collection fantasy name" />
+                  <Input
+                    id="collectionName"
+                    name="collectionName"
+                    value={formData.collectionName}
+                    onChange={handleChange}
+                    placeholder="Collection fantasy name"
+                  />
                 </FormControl>
 
                 <FormControl isRequired mt={4}>
                   <FormLabel>Short Storytelling</FormLabel>
-                  <Input id="shortStorytelling" name="shortStorytelling" value={formData.shortStorytelling } onChange={handleChange} placeholder="Short Storytelling Description" />
+                  <Input
+                    id="shortStorytelling"
+                    name="shortStorytelling"
+                    value={formData.shortStorytelling}
+                    onChange={handleChange}
+                    placeholder="Short Storytelling Description"
+                  />
                 </FormControl>
 
                 <FormControl isRequired mt={4}>
                   <FormLabel>Storytelling Collection</FormLabel>
-                  <Input id="storytellingCollection" name="storytellingCollection" value={formData.storytellingCollection} onChange={handleChange} placeholder="Detailed Story Collection" />
+                  <Input
+                    id="storytellingCollection"
+                    name="storytellingCollection"
+                    value={formData.storytellingCollection}
+                    onChange={handleChange}
+                    placeholder="Detailed Story Collection"
+                  />
                 </FormControl>
 
                 <FormControl isRequired mt={4}>
                   <FormLabel>Total Supply</FormLabel>
-                  <Input id="totalSupply" name="totalSupply" value={formData.totalSupply} onChange={handleChange} placeholder="Tokenomics: Total number of tokens issued" />
+                  <Input
+                    id="totalSupply"
+                    name="totalSupply"
+                    value={formData.totalSupply}
+                    onChange={handleChange}
+                    placeholder="Tokenomics: Total number of tokens issued"
+                  />
                 </FormControl>
 
                 <FormControl isRequired mt={4}>
                   <FormLabel>Distribution</FormLabel>
-                  <Input id="distribution" name="distribution" value={formData.distribution} onChange={handleChange} placeholder="Tokenomics: Percentage or amount of tokens assigned to each item" />
+                  <Input
+                    id="distribution"
+                    name="distribution"
+                    value={formData.distribution}
+                    onChange={handleChange}
+                    placeholder="Tokenomics: Percentage or amount of tokens assigned to each item"
+                  />
                 </FormControl>
 
                 <FormControl isRequired mt={4}>
-                <FormLabel>Utilities</FormLabel>
+                  <FormLabel>Utilities</FormLabel>
                   <CheckboxGroup
                     colorScheme="teal" // ajusta según tus preferencias
                     value={formData.utilities}
@@ -247,48 +272,86 @@ const ColecctionForm = () => {
                 {/* Campo para Nombre del representante o team leader */}
                 <FormControl isRequired mt={4}>
                   <FormLabel>Token Prices</FormLabel>
-                  <Input id="tokenPrices" name="tokenPrices" value={formData.tokenPrices} onChange={handleChange} placeholder="Amount in $USD" />
+                  <Input
+                    id="tokenPrices"
+                    name="tokenPrices"
+                    value={formData.tokenPrices}
+                    onChange={handleChange}
+                    placeholder="Amount in $USD"
+                  />
                 </FormControl>
 
                 {/* Campo para Detalles del representante o team leader */}
                 <FormControl isRequired mt={4}>
                   <FormLabel>Documents to be tokenized</FormLabel>
-                  <Input id="documents" name="documents" value={formData.documents} onChange={handleChange} type="file" placeholder="Upload documents" />
+                  <Input
+                    id="documents"
+                    name="documents"
+                    value={formData.documents}
+                    onChange={handleChange}
+                    type="file"
+                    placeholder="Upload documents"
+                  />
                 </FormControl>
 
                 {/* Campo para Perfil de Linkedin o similar */}
                 <FormControl isRequired mt={4}>
                   <FormLabel>Type of images</FormLabel>
-                  <Input id="typesImages" name="typesImages" value={formData.typesImages} onChange={handleChange} placeholder="Full images o Generated programmatically" />
+                  <Input
+                    id="typesImages"
+                    name="typesImages"
+                    value={formData.typesImages}
+                    onChange={handleChange}
+                    placeholder="Full images o Generated programmatically"
+                  />
                 </FormControl>
 
                 <FormControl isRequired mt={4}>
                   <FormLabel>Upload images</FormLabel>
-                  <Input id="nftImages" name="nftImages" value={formData.nftImages} onChange={handleChange} type="file" placeholder="Load full images or attributes." />
+                  <Input
+                    id="nftImages"
+                    name="nftImages"
+                    value={formData.nftImages}
+                    onChange={handleChange}
+                    type="file"
+                    placeholder="Load full images or attributes."
+                  />
                 </FormControl>
 
                 <FormControl isRequired mt={4}>
                   <FormLabel>Creator</FormLabel>
-                  <Input id="creator" name="creator" value={formData.creator} onChange={handleChange} placeholder="Name, nickname or entity of the creator of the collection." />
+                  <Input
+                    id="creator"
+                    name="creator"
+                    value={formData.creator}
+                    onChange={handleChange}
+                    placeholder="Name, nickname or entity of the creator of the collection."
+                  />
                 </FormControl>
-                
-              <Button type="submit" mt={4} colorScheme="teal">
-                Submit
-              </Button>
-            </form>
-          </Box>
-        </Collapse>
+
+                <Button type="submit" mt={4} colorScheme="teal">
+                  Submit
+                </Button>
+              </form>
+            </Box>
+          </Collapse>
         </Flex>
         <Flex direction="column" align="center">
-          <Tooltip label="Your Startup and R&D Project must be registered on the platform and have been approved in order to complete this form." textColor="#FFFFFF" bg="#000000" fontSize="md" placement="right-start">
+          <Tooltip
+            label="Your Startup and R&D Project must be registered on the platform and have been approved in order to complete this form."
+            textColor="#FFFFFF"
+            bg="#000000"
+            fontSize="md"
+            placement="right-start"
+          >
             <Box>
               <IoInformationCircleOutline size={25} color="#737373" />
             </Box>
           </Tooltip>
-      </Flex>
+        </Flex>
       </Flex>
     </Center>
-  );
-};
+  )
+}
 
-export default ColecctionForm;
+export default ColecctionForm
