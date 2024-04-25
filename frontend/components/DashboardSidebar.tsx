6e9acc1e9edff2f71f27231068c2784595966b98
Daniel Benitez <da8y_01@yahoo.com>
2024-04-25 08:53:53 -0500

    fix: Set overflow scroll on SidebarContent.

--- frontend/components/DashboardSidebar.tsx
+++ frontend/components/DashboardSidebar.tsx
@@ -171,6 +171,7 @@ const SidebarContent = ({
       borderRightColor={useColorModeValue("gray.200", "gray.700")}
       w={{ base: "full", md: 60 }}
       pos="fixed"
+      overflow="scroll"
       h="full"
       {...rest}
     >
